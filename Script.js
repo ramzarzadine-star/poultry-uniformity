b062bbe44b94a652c4005bf35d29e36aa58b5a83
ramzarzadine-star <ramzarzadine@gmail.com>
2026-08-14 04:07:49 +0330

    Update Script.js

--- Script.js
+++ Script.js
@@ -2,7 +2,8 @@ let weightChart;
 let cvChart;
 
 
-function persianToEnglish(str){
+
+function persianNumber(str){
 
 return str.replace(/[۰-۹]/g,function(x){
 
@@ -14,9 +15,12 @@ return "۰۱۲۳۴۵۶۷۸۹".indexOf(x);
 
 
 
+
+
 function parseWeights(text){
 
-text=persianToEnglish(text);
+
+text=persianNumber(text);
 
 
 return text
@@ -27,52 +31,57 @@ return text
 
 .map(Number)
 
-.filter(x=>Number.isFinite(x));
+.filter(x=>Number.isFinite(x)&&x>0);
 
 
 }
 
 
 
-function avg(a){
 
-return a.reduce((x,y)=>x+y,0)/a.length;
+
+function average(arr){
+
+return arr.reduce((a,b)=>a+b,0)/arr.length;
 
 }
 
 
 
-function calculateSD(a,m){
+
+
+function standard(arr,mean){
 
 let sum=0;
 
-for(let i=0;i<a.length;i++){
 
-sum += Math.pow(a[i]-m,2);
+arr.forEach(x=>{
+
+sum+=Math.pow(x-mean,2);
+
+});
+
+
+return Math.sqrt(sum/(arr.length-1));
+
+}
+
+
+
+
+
+function uniformity(arr,mean,p){
+
+let low=mean*(1-p);
+
+let high=mean*(1+p);
+
+
+return arr.filter(x=>x>=low&&x<=high).length/arr.length*100;
 
 }
 
-return Math.sqrt(sum/(a.length-1));
 
-}
-
-
-
-
-function uniformity(a,m,p){
-
-let min=m*(1-p);
-
-let max=m*(1+p);
-
-
-return a.filter(
-
-x=>x>=min&&x<=max
-
-).length/a.length*100;
-
-}
 
 
 
@@ -81,16 +90,17 @@ x=>x>=min&&x<=max
 function calculate(){
 
 
-let input=document.getElementById("weightsInput").value;
+let weights=parseWeights(
 
+document.getElementById("weightsInput").value
 
-let weights=parseWeights(input);
+);
 
 
 
 if(weights.length<2){
 
-alert("حداقل دو وزن وارد کنید");
+alert("وزن وارد کنید");
 
 return;
 
@@ -98,128 +108,67 @@ return;
 
 
 
-let mean=avg(weights);
+let mean=average(weights);
 
 
-let deviation=calculateSD(weights,mean);
+let sd=standard(weights,mean);
 
 
-let cv=(deviation/mean)*100;
+let cv=(sd/mean)*100;
 
 
-let u10=uniformity(weights,mean,0.10);
+let u10=uniformity(weights,mean,.1);
 
 
-let u15=uniformity(weights,mean,0.15);
+let u15=uniformity(weights,mean,.15);
 
 
 
-document.getElementById("count").innerHTML=weights.length;
+
+document.getElementById("sample").innerHTML=weights.length;
+
 
 document.getElementById("mean").innerHTML=mean.toFixed(1);
 
-document.getElementById("min").innerHTML=Math.min.apply(null,weights);
 
-document.getElementById("max").innerHTML=Math.max.apply(null,weights);
+document.getElementById("min").innerHTML=Math.min(...weights);
 
-document.getElementById("sd").innerHTML=deviation.toFixed(1);
+
+document.getElementById("max").innerHTML=Math.max(...weights);
+
+
+document.getElementById("sd").innerHTML=sd.toFixed(1);
+
 
 document.getElementById("cv").innerHTML=cv.toFixed(2)+"%";
 
+
 document.getElementById("u10").innerHTML=u10.toFixed(1)+"%";
+
 
 document.getElementById("u15").innerHTML=u15.toFixed(1)+"%";
 
 
-drawUniformity(weights,mean);
+
+drawWeight(weights);
+
 
 drawCV(cv);
-}
-
-
-function drawCV(value){
-
-
-if(cvChart){
-
-cvChart.destroy();
-
-}
-
-
-
-cvChart = new Chart(
-
-document.getElementById("cvChart"),
-
-{
-
-type:"line",
-
-data:{
-
-labels:["گله"],
-
-datasets:[{
-
-label:"CV (%)",
-
-data:[value],
-
-borderWidth:3,
-
-fill:false,
-
-tension:0.3
-
-}]
-
-},
-
-
-options:{
-
-responsive:true,
-
-scales:{
-
-y:{
-
-beginAtZero:true,
-
-title:{
-
-display:true,
-
-text:"درصد CV"
-
-}
-
-}
-
-}
-
-}
-
-}
-
-);
 
 
 }
 
-function drawUniformity(data,mean){
 
 
-if(weightChart){
 
+
+
+
+function drawWeight(data){
+
+
+if(weightChart)
 weightChart.destroy();
-
-}
-
-
-
-let value=uniformity(data,mean,0.10);
 
 
 
@@ -233,11 +182,63 @@ type:"line",
 
 data:{
 
-labels:["گله"],
+labels:data.map((x,i)=>"جوجه "+(i+1)),
 
 datasets:[{
 
-label:"یکنواختی ±10%",
+label:"وزن",
+
+data:data,
+
+borderWidth:3,
+
+fill:false,
+
+tension:.3
+
+}]
+
+}
+
+}
+
+);
+
+
+}
+
+
+
+
+
+
+
+
+
+function drawCV(value){
+
+
+if(cvChart)
+
+cvChart.destroy();
+
+
+
+cvChart=new Chart(
+
+document.getElementById("cvChart"),
+
+{
+
+type:"line",
+
+data:{
+
+labels:["CV"],
+
+datasets:[{
+
+label:"CV درصد",
 
 data:[value],
 
@@ -245,7 +246,7 @@ borderWidth:3,
 
 fill:false,
 
-tension:0.3
+tension:.3
 
 }]
 
@@ -254,23 +255,11 @@ tension:0.3
 
 options:{
 
-responsive:true,
-
 scales:{
 
 y:{
 
-beginAtZero:true,
-
-max:100,
-
-title:{
-
-display:true,
-
-text:"درصد یکنواختی"
-
-}
+beginAtZero:true
 
 }
 
